31541db4455aaa2351a2aeb387d5d10a0b87819b
LHJ <leehj401@gmail.com>
2017-11-15 11:28:46 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,7 +4,9 @@ window =Tk()
 
 b1 = Button(window, text="HYEJEONGLEE")
 b2 = Button(window, text="60162169")
+b3 = Button(window, text="0401")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
